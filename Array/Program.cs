27d4572a3bf2fcf56289034c5877d004f9d4b5cf
siyad <siyad@outlook.com>
2023-11-19 09:57:11 +0000

    Added some comments to array program

--- Array/Program.cs
+++ Array/Program.cs
@@ -1,12 +1,13 @@
 ﻿//Array declaration and initialization
-int[] a1; //declaration
-a1 = new int[5]; //Initialization
+int[] a1; // Just declaration only, not allocated memory
+a1 = new int[5]; //This syntax is for declaration and allocated memory for it
 
 int[] a2 = new int[2];
-int[] a3 = new int[3] { 1, 2, 3 };
+int[] a3 = new int[3] { 1, 2, 3 }; //Declaration & initialization of values
 
-int[] a4 = { 1, 32 };
+int[] a4 = { 1, 32 }; // This is the short hand syntax for declaring and initializing
 
+//iterating the array with foreach loop
 foreach (int i in a1)
 {
     Console.Write($"{i} ", i);
@@ -20,12 +21,12 @@ foreach (int i in a3)
 Console.WriteLine();
 
 
-
+//Array is a type which we can directly use to create and initialize values
 Array myArray = new int[5];
 Array myArray2 = Array.CreateInstance(typeof(int), 5);
 myArray2.SetValue(1, 0);
 
-//one based instance. 
+//one based instance. We can change indexing of array from 0 to 1 even though it is not useful!
 Array oneBasedArray = Array.CreateInstance(typeof(int), new[] {5}, new[] { 1 }); //Non zero arrays are not CLS complaint 
 oneBasedArray.SetValue(2, 1);
 oneBasedArray.SetValue(23, 2);
@@ -33,7 +34,7 @@ oneBasedArray.SetValue(24, 3);
 oneBasedArray.SetValue(25, 4);
 oneBasedArray.SetValue(27, 5);
 
-
+//GetLowerBound(0) and GetUpperBound(0) can give lower index and higher index
 Console.Write($"Starting index {oneBasedArray.GetLowerBound(0)}");
 Console.WriteLine("\nMulti dimensional arrays");
 
